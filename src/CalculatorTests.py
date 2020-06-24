644a2c555755af6be64507fd027ff052543fa2e4
vd265 <60578444+vd265@users.noreply.github.com>
2020-06-24 14:34:00 -0400

    Feature: Setup calculator with add method

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -11,5 +11,9 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertEqual(calculator.result, 4)
 
+    def test_add_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2, 2), 4)
+
 if __name__ == '__main__':
         unittest.main()
