3d9b9ab7efa5299965e04d5fd427b8a10833b810
vd265 <60578444+vd265@users.noreply.github.com>
2020-06-24 17:15:48 -0400

    Feature: Setup subtraction csv file

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,25 +1,25 @@
 import unittest
 from Calculator import Calculator
+from CsvReader import CsvReader
+from pprint import pprint
+
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 0)
+    def test_subtraction(self):
+        test_data = CsvReader('/src/subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
-    def test_add_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2, 2), 4)
-        self.assertEqual(calculator.result, 4)
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
-    def test_subtract_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(2, 2), 0)
-        self.assertEqual(calculator.result, 0)
 
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
