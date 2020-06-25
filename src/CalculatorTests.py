a5f9aded560264ffe77fbedddd5aca32abfcddd3
vd265 <60578444+vd265@users.noreply.github.com>
2020-06-24 20:44:43 -0400

    Feature: Setup csv files

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -11,6 +11,12 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
+    def test_addition(self):
+        test_data = CsvReader('/src/Addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
     def test_subtraction(self):
         test_data = CsvReader('/src/subtraction.csv').data
         for row in test_data:
